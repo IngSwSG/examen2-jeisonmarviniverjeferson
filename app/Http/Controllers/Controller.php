ca927e11efb5b813258de2de163f3f85f2429822
JeisonVR97 <jeison17.cr@gmail.com>
2024-07-03 16:36:17 -0600

    Creacion Metodos insertar, crear y rutas de los metodos

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,46 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Material;
 
 abstract class Controller
 {
-    //
+    public function Insertar(Request $request)
+    {
+        $request->validate([
+            'unidadMedida' => 'required|string|max:255',
+            'descripcion' => 'required|string|max:255',
+            'ubicacion' => 'required|string|max:255',
+            'categoria' => 'required|exists:categorias,idCategoria',
+        ]);
+
+        $material = Material::create([
+            'unidadMedida' => $request->input('unidadMedida'),
+            'descripcion' => $request->input('descripcion'),
+            'ubicacion' => $request->input('ubicacion'),
+            'categoria' => $request->input('categoria'),
+        ]);
+
+        return response()->json($material, 201);
+    }
+
+    public function Actualizar(Request $request, $id)
+    {
+        $request->validate([
+            'unidadMedida' => 'required|string|max:255',
+            'descripcion' => 'required|string|max:255',
+            'ubicacion' => 'required|string|max:255',
+            'categoria' => 'required|exists:categorias,idCategoria',
+        ]);
+
+        $material = Material::findOrFail($id);
+        $material->update([
+            'unidadMedida' => $request->input('unidadMedida'),
+            'descripcion' => $request->input('descripcion'),
+            'ubicacion' => $request->input('ubicacion'),
+            'categoria' => $request->input('categoria'),
+        ]);
+
+        return response()->json($material, 200);
+    }
 }
