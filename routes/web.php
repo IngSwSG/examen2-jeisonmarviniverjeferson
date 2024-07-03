f7bf5915025f2bce6b47afa0090c36986258439e
JeisonVR97 <jeison17.cr@gmail.com>
2024-07-03 16:31:49 -0600

    Creacion Metodos insertar, crear y rutas de los metodos

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/materiales', [MaterialController::class, 'Insertar']);
+Route::put('/materiales/{id}', [MaterialController::class, 'Actualizar']);
